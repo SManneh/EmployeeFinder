b61dcbd09d68116b6e51cb16144e744df3726790
Nabou <Nabou@Sainabou.local>
2018-10-23 22:48:52 -0500

    home html

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.get("/survey", function(req, res){
     res.sendFile(path.join(__dirname , "/../public/survey.html"));
 })
-router.get("/", function(req, res){
+router.get("/home", function(req, res){
     res.sendFile(path.join(__dirname , "/../public/home.html"));
 })
 
