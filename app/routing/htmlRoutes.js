84f12017f9b0db14ab1f4dbe3c541f7c2f56533c
Nabou <Nabou@Sainabou.local>
2018-10-23 21:46:23 -0500

    writing survey code

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -11,7 +11,7 @@ router.get("/survey", function(req, res){
     res.sendFile(path.join(__dirname , "/../public/survey.html"));
 })
 router.get("/", function(req, res){
-    res.sendFile();
+    res.sendFile(path.join(__dirname , "/../public/home.html"));
 })
 
 module.exports = router;
